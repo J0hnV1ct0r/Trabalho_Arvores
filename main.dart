ecd78888a2c40d7501a7346005f19650a9de4b3b
João Victor de S. Albuquerque <84153487+J0hnV1ct0r@users.noreply.github.com>
2022-11-17 23:35:30 +0000

    Primeira versão da Classe  Galho

---
+++ main.dart
@@ -0,0 +1,18 @@
+class Galho{
+  var valor;
+  var folhaEsquerda;
+  var folhaDireita;
+  void set_folhaEsquerda(var valor){
+    this.folhaEsquerda = Galho();
+    this.folhaEsquerda.valor = valor;
+  }
+  void set_folhaDireita(var valor){
+    this.folhaDireita = Galho();
+    this.folhaDireita.valor = valor;
+  }
+}
+
+
+void main() {
+	print('Hello World!');
+}
